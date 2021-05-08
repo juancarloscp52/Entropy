bad5bb28b54da1a1b9d59c46aee9a5e9e310b544
jc <juancarloscp52@gmail.com>
2021-05-08 20:08:51 +0200

    Mega Explosion Event is a bit more lethal...

--- src/main/java/me/juancarloscp52/entropy/events/db/ExtremeExplosionEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/ExtremeExplosionEvent.java
@@ -3,11 +3,12 @@ package me.juancarloscp52.entropy.events.db;
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.world.explosion.Explosion;
 
 public class ExtremeExplosionEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
-        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(serverPlayerEntity -> serverPlayerEntity.getServerWorld().createExplosion(serverPlayerEntity, serverPlayerEntity.getX(), serverPlayerEntity.getY(), serverPlayerEntity.getZ(), 7.5f, true, null));
+        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(serverPlayerEntity -> serverPlayerEntity.getServerWorld().createExplosion(serverPlayerEntity, serverPlayerEntity.getX(), serverPlayerEntity.getY(), serverPlayerEntity.getZ(), 10f, true, Explosion.DestructionType.BREAK));
     }
 }
