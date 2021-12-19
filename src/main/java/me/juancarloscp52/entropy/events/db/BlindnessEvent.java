dc403428bd715978c60b56ef37f8e1c38f7867df
jc <juancarloscp52@gmail.com>
2021-12-19 20:03:42 +0100

    Adjust blindnessEvent map

--- src/main/java/me/juancarloscp52/entropy/events/db/BlindnessEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/BlindnessEvent.java
@@ -27,6 +27,6 @@ public class BlindnessEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
-        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(serverPlayerEntity -> serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, (int) (Entropy.getInstance().settings.baseEventDuration*1.25))));
+        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(serverPlayerEntity -> serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS,(Entropy.getInstance().settings.baseEventDuration))));
     }
 }
