db44d22d76f188be466232d49648b335887a0a4d
jc <juancarloscp52@gmail.com>
2022-03-07 17:22:40 +0100

    Fix autojumo on nojumpEvent (Fixes #7)

---
+++ src/main/java/me/juancarloscp52/entropy/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 juancarloscp52
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package me.juancarloscp52.entropy.mixin;
+
+import me.juancarloscp52.entropy.Variables;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+
+    @Inject(method = "isAutoJumpEnabled", at=@At("RETURN"), cancellable = true)
+    private void changeAutoJump(CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(cir.getReturnValue() && !Variables.noJump);
+    }
+
+}
