2cc125f05c7fb35399ade05f569845b2f3c572b9
jc <juancarloscp52@gmail.com>
2022-11-22 18:39:32 +0100

    Use Built-in minecraft uri handling

--- src/main/java/me/juancarloscp52/entropy/client/integrations/youtube/YoutubeApi.java
+++ src/main/java/me/juancarloscp52/entropy/client/integrations/youtube/YoutubeApi.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
+import net.minecraft.util.Util;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
@@ -145,26 +146,8 @@ public class YoutubeApi {
             uri.addParameter("code_challenge_method", "S256");
             var url = uri.build().toString();
 
-            var os = System.getProperty("os.name").toLowerCase();
-            if (os.indexOf("win") >= 0)
-                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-            else if (os.indexOf("mac") >= 0)
-                Runtime.getRuntime().exec("open " + url);
-            else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
-                var rt = Runtime.getRuntime();
-                String[] browsers = { "google-chrome", "firefox", "mozilla", "epiphany", "konqueror", "netscape",
-                        "opera", "links", "lynx" };
-
-                var cmd = new StringBuffer();
-                for (int i = 0; i < browsers.length; i++)
-                    if (i == 0)
-                        cmd.append(String.format("%s \"%s\"", browsers[i], url));
-                    else
-                        cmd.append(String.format(" || %s \"%s\"", browsers[i], url));
-                // If the first didn't work, try the next browser and so on
-
-                rt.exec(new String[] { "sh", "-c", cmd.toString() });
-            }
+            LOGGER.info("Youtube authorization: Opening browser authorization");
+            Util.getOperatingSystem().open(url);
 
         } catch (Exception ex) {
             LOGGER.error(ex.getMessage());
