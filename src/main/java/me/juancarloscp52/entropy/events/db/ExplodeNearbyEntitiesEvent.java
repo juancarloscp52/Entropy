8bb914a559a2e70d6d1953804bf34103e12694f9
jc <juancarloscp52@gmail.com>
2022-11-24 20:00:02 +0100

    Prevent Ender dragon from being directly killed by Explode Nearby Entities Event

--- src/main/java/me/juancarloscp52/entropy/events/db/ExplodeNearbyEntitiesEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/ExplodeNearbyEntitiesEvent.java
@@ -20,6 +20,7 @@ package me.juancarloscp52.entropy.events.db;
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.explosion.Explosion;
 
@@ -29,7 +30,8 @@ public class ExplodeNearbyEntitiesEvent extends AbstractInstantEvent {
     public void init() {
         PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(serverPlayerEntity -> serverPlayerEntity.getEntityWorld().getOtherEntities(serverPlayerEntity, new Box(serverPlayerEntity.getBlockPos().add(70, 70, 70), serverPlayerEntity.getBlockPos().add(-70, -70, -70))).forEach(entity -> {
             entity.getEntityWorld().createExplosion(entity, entity.getX(), entity.getY() + 1f, entity.getZ(), 2.1f, Explosion.DestructionType.DESTROY);
-            entity.kill();
+            if(!(entity instanceof EnderDragonEntity))
+                entity.kill();
         }));
     }
 
