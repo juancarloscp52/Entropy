10073d745f5eb5d492f08966cdf250f4a230dd5b
Kanawanagasaki <kanawanagasaki@gmail.com>
2022-11-26 00:15:46 +0300

    [fix] slimes

--- src/main/java/me/juancarloscp52/entropy/events/db/SlimePyramidEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/SlimePyramidEvent.java
@@ -16,23 +16,25 @@ public class SlimePyramidEvent extends AbstractInstantEvent {
     public void init() {
         for (var player : PlayerLookup.all(Entropy.getInstance().eventHandler.server)) {
 
-            var slimeSize = player.getRandom().nextInt(3) + 4;
+            var slimeSize = player.getRandom().nextInt(4) + 4;
 
             var angle = player.getRandom().nextDouble() * Math.PI;
             var cos = Math.cos(angle);
             var sin = Math.sin(angle);
+            var addX = cos * 5;
+            var addY = sin * 5;
 
             var slime = new SlimeEntity(EntityType.SLIME, player.getWorld());
             slime.setSize(slimeSize, true);
-            slime.setPosition(player.getPos().add(cos * 5, 0, sin * 5));
+            slime.setPosition(player.getPos().add(addX, 0, addY));
             player.getWorld().spawnEntity(slime);
 
-            // > 1 coz the top slime of size 0 is always dying
             while (slimeSize > 1) {
                 slimeSize--;
 
                 var slime2 = new SlimeEntity(EntityType.SLIME, player.getWorld());
                 slime2.setSize(slimeSize, true);
+                slime2.setPosition(player.getPos().add(addX, 0, addY));
                 player.getWorld().spawnEntity(slime2);
 
                 slime2.startRiding(slime, true);
