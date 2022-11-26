d6e76345452147ecdaf4f934b939ff36c91be1e0
jc <juancarloscp52@gmail.com>
2022-11-26 17:10:45 +0100

    Explode and Ignite nearby entities no longer affects other players.

--- src/main/java/me/juancarloscp52/entropy/events/db/ExplodeNearbyEntitiesEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/ExplodeNearbyEntitiesEvent.java
@@ -20,6 +20,7 @@ package me.juancarloscp52.entropy.events.db;
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
 import net.minecraft.world.explosion.Explosion;
 
@@ -29,7 +30,7 @@ public class ExplodeNearbyEntitiesEvent extends AbstractInstantEvent {
     public void init() {
         Entropy.getInstance().eventHandler.getActivePlayers().forEach(serverPlayerEntity -> serverPlayerEntity.getEntityWorld().getOtherEntities(serverPlayerEntity, new Box(serverPlayerEntity.getBlockPos().add(70, 70, 70), serverPlayerEntity.getBlockPos().add(-70, -70, -70))).forEach(entity -> {
             entity.getEntityWorld().createExplosion(entity, entity.getX(), entity.getY() + 1f, entity.getZ(), 2.1f, Explosion.DestructionType.DESTROY);
-            if(!(entity instanceof EnderDragonEntity))
+            if(!(entity instanceof EnderDragonEntity || entity instanceof PlayerEntity))
                 entity.kill();
         }));
     }
--- src/main/java/me/juancarloscp52/entropy/events/db/IgniteNearbyEntitiesEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/IgniteNearbyEntitiesEvent.java
@@ -19,13 +19,17 @@ package me.juancarloscp52.entropy.events.db;
 
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Box;
 
 public class IgniteNearbyEntitiesEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
-        Entropy.getInstance().eventHandler.getActivePlayers().forEach(serverPlayerEntity -> serverPlayerEntity.getWorld().getOtherEntities(serverPlayerEntity, new Box(serverPlayerEntity.getBlockPos().add(50, 50, 50), serverPlayerEntity.getBlockPos().add(-50, -50, -50))).forEach(entity -> entity.setOnFireFor(30)));
+        Entropy.getInstance().eventHandler.getActivePlayers().forEach(serverPlayerEntity -> serverPlayerEntity.getWorld().getOtherEntities(serverPlayerEntity, new Box(serverPlayerEntity.getBlockPos().add(50, 50, 50), serverPlayerEntity.getBlockPos().add(-50, -50, -50))).forEach(entity -> {
+            if(!(entity instanceof PlayerEntity))
+                entity.setOnFireFor(30);
+        }));
     }
 
 }
