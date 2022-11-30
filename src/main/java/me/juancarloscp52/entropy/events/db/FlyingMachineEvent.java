a14c57b3aaed7dcb0b4ccd8e3ead3260e613bf8d
Kanawanagasaki <kanawanagasaki@gmail.com>
2022-11-30 07:42:36 +0300

    [feat] Flying Machine Event

---
+++ src/main/java/me/juancarloscp52/entropy/events/db/FlyingMachineEvent.java
@@ -0,0 +1,91 @@
+package me.juancarloscp52.entropy.events.db;
+
+import me.juancarloscp52.entropy.Entropy;
+import me.juancarloscp52.entropy.events.AbstractInstantEvent;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
+public class FlyingMachineEvent extends AbstractInstantEvent {
+
+    @Override
+    public void init() {
+        for (var player : Entropy.getInstance().eventHandler.getActivePlayers()) {
+            if(player.getRandom().nextInt(2) == 0)
+                spawnEastWest(player.getWorld(), player.getBlockPos().add(1, 2, -4));
+            else
+                spawnNorthSouth(player.getWorld(), player.getBlockPos().add(-4, 2, 1));
+        }
+    }
+
+    private void spawnEastWest(ServerWorld world, BlockPos startPos) {
+        for (int ix = 0; ix < 4; ix++) {
+            for (int iy = 0; iy < 4; iy++) {
+                for (int iz = 0; iz < 8; iz++) {
+                    var blockPos = startPos.add(ix, iy, iz);
+                    var currentBlock = world.getBlockState(blockPos);
+                    if (currentBlock.getBlock() == Blocks.BEDROCK ||
+                            currentBlock.getBlock() == Blocks.END_PORTAL_FRAME ||
+                            currentBlock.getBlock() == Blocks.END_PORTAL)
+                        return; // Do not spawn flyting machine at all
+
+                    // Make sure flying machine have space to spawn
+                    world.setBlockState(blockPos, Blocks.AIR.getDefaultState());
+                }
+            }
+        }
+
+        world.setBlockState(startPos.add(1, 1, 3), Blocks.STICKY_PISTON.getDefaultState().with(FacingBlock.FACING, Direction.EAST));
+        world.setBlockState(startPos.add(1, 1, 4), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(1, 1, 5), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(1, 1, 6), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.SOUTH));
+        
+        world.setBlockState(startPos.add(2, 1, 1), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.NORTH));
+        world.setBlockState(startPos.add(2, 1, 2), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(2, 1, 3), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(2, 1, 4), Blocks.STICKY_PISTON.getDefaultState().with(FacingBlock.FACING, Direction.WEST));
+        
+        world.setBlockState(startPos.add(1, 2, 4), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.SOUTH));
+        world.setBlockState(startPos.add(1, 2, 5), Blocks.REDSTONE_LAMP.getDefaultState());
+        
+        world.setBlockState(startPos.add(2, 2, 2), Blocks.REDSTONE_LAMP.getDefaultState());
+        world.setBlockState(startPos.add(2, 2, 3), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.NORTH));
+    }
+
+    private void spawnNorthSouth(ServerWorld world, BlockPos startPos) {
+        for (int ix = 0; ix < 8; ix++) {
+            for (int iy = 0; iy < 4; iy++) {
+                for (int iz = 0; iz < 4; iz++) {
+                    var blockPos = startPos.add(ix, iy, iz);
+                    var currentBlock = world.getBlockState(blockPos);
+                    if (currentBlock.getBlock() == Blocks.BEDROCK ||
+                            currentBlock.getBlock() == Blocks.END_PORTAL_FRAME ||
+                            currentBlock.getBlock() == Blocks.END_PORTAL)
+                        return; // Do not spawn flyting machine at all
+
+                    // Make sure flying machine have space to spawn
+                    world.setBlockState(blockPos, Blocks.AIR.getDefaultState());
+                }
+            }
+        }
+
+        world.setBlockState(startPos.add(3, 1, 1), Blocks.STICKY_PISTON.getDefaultState().with(FacingBlock.FACING, Direction.SOUTH));
+        world.setBlockState(startPos.add(4, 1, 1), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(5, 1, 1), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(6, 1, 1), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.EAST));
+        
+        world.setBlockState(startPos.add(1, 1, 2), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.WEST));
+        world.setBlockState(startPos.add(2, 1, 2), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(3, 1, 2), Blocks.SLIME_BLOCK.getDefaultState());
+        world.setBlockState(startPos.add(4, 1, 2), Blocks.STICKY_PISTON.getDefaultState().with(FacingBlock.FACING, Direction.NORTH));
+        
+        world.setBlockState(startPos.add(4, 2, 1), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.EAST));
+        world.setBlockState(startPos.add(5, 2, 1), Blocks.REDSTONE_LAMP.getDefaultState());
+        
+        world.setBlockState(startPos.add(2, 2, 2), Blocks.REDSTONE_LAMP.getDefaultState());
+        world.setBlockState(startPos.add(3, 2, 2), Blocks.OBSERVER.getDefaultState().with(FacingBlock.FACING, Direction.WEST));
+    }
+
+}
