decf39046453aaf4b7edde221c4621b8643eac8b
jc <juancarloscp52@gmail.com>
2022-12-21 17:27:54 +0100

    Use client bossbar

---
+++ src/main/java/me/juancarloscp52/entropy/mixin/BossBarHudAccessor.java
@@ -0,0 +1,17 @@
+package me.juancarloscp52.entropy.mixin;
+
+import net.minecraft.client.gui.hud.BossBarHud;
+import net.minecraft.client.gui.hud.ClientBossBar;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Mixin(BossBarHud.class)
+public interface BossBarHudAccessor {
+
+    @Accessor
+    Map<UUID, ClientBossBar> getBossBars();
+
+}
