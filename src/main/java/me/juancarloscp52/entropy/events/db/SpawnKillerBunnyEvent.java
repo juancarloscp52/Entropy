50be1478f713b5b8572b1c47d45dfe4dbe41d68a
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-01-11 14:02:06 +0100

    add spawn killer bunny event

---
+++ src/main/java/me/juancarloscp52/entropy/events/db/SpawnKillerBunnyEvent.java
@@ -0,0 +1,22 @@
+package me.juancarloscp52.entropy.events.db;
+
+import me.juancarloscp52.entropy.Entropy;
+import me.juancarloscp52.entropy.events.AbstractInstantEvent;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.passive.RabbitEntity.RabbitType;
+import net.minecraft.util.math.random.Random;
+
+public class SpawnKillerBunnyEvent extends AbstractInstantEvent {
+    @Override
+    public void init() {
+        Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> {
+            Random random = player.getRandom();
+            int bunnyAmount = random.nextBetween(1, 6);
+
+            for(int i = 0; i < bunnyAmount; i++) {
+                EntityType.RABBIT.spawn(player.getWorld(), null, rabbit -> rabbit.setVariant(RabbitType.EVIL), player.getBlockPos().add(random.nextBetween(-4, 4), random.nextInt(2), random.nextBetween(-4, 4)), SpawnReason.SPAWN_EGG, false, false);
+            }
+        });
+    }
+}
