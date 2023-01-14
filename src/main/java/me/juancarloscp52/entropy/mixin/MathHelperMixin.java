923a5dd1986acd50f4170b58bb5d34479842e616
jc <juancarloscp52@gmail.com>
2023-01-14 18:07:40 +0100

    Do not replace return values when event is not active

--- src/main/java/me/juancarloscp52/entropy/mixin/MathHelperMixin.java
+++ src/main/java/me/juancarloscp52/entropy/mixin/MathHelperMixin.java
@@ -11,26 +11,20 @@ import net.minecraft.util.math.MathHelper;
 @Mixin(MathHelper.class)
 public class MathHelperMixin {
     @Inject(method = "Lnet/minecraft/util/math/MathHelper;lerp(FFF)F", at = @At("HEAD"), cancellable = true)
-    private static void lerp(float delta, float start, float end, CallbackInfoReturnable cir) {
+    private static void lerp(float delta, float start, float end, CallbackInfoReturnable<Float> cir) {
         if(Variables.stuttering)
             cir.setReturnValue(start);
-        else
-            cir.setReturnValue(start + delta * (end - start));
     }
 
     @Inject(method = "Lnet/minecraft/util/math/MathHelper;lerp(DDD)D", at = @At("HEAD"), cancellable = true)
-    private static void lerp(double delta, double start, double end, CallbackInfoReturnable cir) {
+    private static void lerp(double delta, double start, double end, CallbackInfoReturnable<Double> cir) {
         if(Variables.stuttering)
             cir.setReturnValue(start);
-        else
-            cir.setReturnValue(start + delta * (end - start));
     }
 
     @Inject(method = "Lnet/minecraft/util/math/MathHelper;lerpAngleDegrees(FFF)F", at = @At("HEAD"), cancellable = true)
-    private static void lerpAngleDegrees(float delta, float start, float end, CallbackInfoReturnable cir) {
+    private static void lerpAngleDegrees(float delta, float start, float end, CallbackInfoReturnable<Float> cir) {
         if(Variables.stuttering)
             cir.setReturnValue(start);
-        else
-            cir.setReturnValue(start + delta * MathHelper.wrapDegrees(end - start));
     }
 }
