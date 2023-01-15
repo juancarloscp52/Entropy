620b59fa92878214af8e17bf9abfdcbb3a0adcaa
jc <juancarloscp52@gmail.com>
2023-01-15 12:01:20 +0100

    Do not apply events to inactive players.

--- src/main/java/me/juancarloscp52/entropy/events/db/ArmorCurseEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/ArmorCurseEvent.java
@@ -6,14 +6,13 @@ package me.juancarloscp52.entropy.events.db;
 
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.enchantment.Enchantments;
 
 public class ArmorCurseEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
-        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(
+        Entropy.getInstance().eventHandler.getActivePlayers().forEach(
                 serverPlayerEntity -> {
                     serverPlayerEntity.getInventory().armor.forEach(item -> {
                         item.addEnchantment(Enchantments.BINDING_CURSE, 1);
--- src/main/java/me/juancarloscp52/entropy/events/db/WardenEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/WardenEvent.java
@@ -6,7 +6,6 @@ package me.juancarloscp52.entropy.events.db;
 
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -18,7 +17,7 @@ public class WardenEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
-        PlayerLookup.all(Entropy.getInstance().eventHandler.server).forEach(
+        Entropy.getInstance().eventHandler.getActivePlayers().forEach(
                 serverPlayerEntity -> {
                     WardenEntity warden = EntityType.WARDEN.spawn(serverPlayerEntity.getWorld(), serverPlayerEntity.getBlockPos(), SpawnReason.SPAWN_EGG);
                     if(warden!=null) {
