e6c380204b2fb865397f317c9d3e1eb3c505c943
Kanawanagasaki <kanawanagasaki@gmail.com>
2023-01-31 11:19:16 +0300

    [feat] Vex event now respect world difficulty

--- src/main/java/me/juancarloscp52/entropy/events/db/VexAttackEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/VexAttackEvent.java
@@ -8,17 +8,20 @@ import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.world.Difficulty;
 
 public class VexAttackEvent extends AbstractInstantEvent {
 
     @Override
     public void init() {
         for (var serverPlayerEntity : Entropy.getInstance().eventHandler.getActivePlayers()) {
+            var world = serverPlayerEntity.getWorld();
             var random = serverPlayerEntity.getRandom();
-            var vexAmount = random.nextInt(2) + 1;
+            var difficulty = world.getDifficulty();
+            var vexAmount = random.nextInt(difficulty == Difficulty.HARD ? 4 : difficulty == Difficulty.NORMAL ? 3 : 2) + 1;
             for (int i = 0; i < vexAmount; i++) {
                 var spawnPos = serverPlayerEntity.getBlockPos().add(random.nextInt(25) - 12, random.nextInt(5) - 2, random.nextInt(25) - 12);
-                EntityType.VEX.spawn(serverPlayerEntity.getWorld(), spawnPos, SpawnReason.SPAWN_EGG);
+                EntityType.VEX.spawn(world, spawnPos, SpawnReason.SPAWN_EGG);
             }
         }
     }
