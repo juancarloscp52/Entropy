a19bd364ec5e6f82832f23edeb24c2d244e4c16b
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-02-21 19:28:56 +0100

    add tag to define which entities cannot be invisible
    also reduce duplicate code in invisibility events

--- src/main/java/me/juancarloscp52/entropy/events/db/InvisibleHostileMobsEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/InvisibleHostileMobsEvent.java
@@ -4,56 +4,12 @@
 
 package me.juancarloscp52.entropy.events.db;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import me.juancarloscp52.entropy.Entropy;
-import me.juancarloscp52.entropy.events.AbstractTimedEvent;
-import me.juancarloscp52.entropy.server.ServerEventHandler;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.Monster;
-import net.minecraft.server.world.ServerWorld;
 
-public class InvisibleHostileMobsEvent extends AbstractTimedEvent {
-
+public class InvisibleHostileMobsEvent extends InvisibleEveryoneEvent {
     @Override
-    public void init() {
+    public boolean shouldBeInvisible(Entity entity) {
+        return entity instanceof Monster && super.shouldBeInvisible(entity);
     }
-
-    @Override
-    public void tick() {
-        ServerEventHandler eventHandler = Entropy.getInstance().eventHandler;
-        List<ServerWorld> worlds = new ArrayList<>();
-        for(var player : eventHandler.getActivePlayers())
-            if(!worlds.contains(player.getWorld()))
-                worlds.add(player.getWorld());
-        for(var world : worlds)
-            for(var entity : world.iterateEntities())
-                if(entity instanceof Monster && entity instanceof LivingEntity)
-                    ((LivingEntity)entity).addStatusEffect(new StatusEffectInstance(StatusEffects.INVISIBILITY, 2));
-        super.tick();
-    }
-
-    @Override
-    public void end() {
-        this.hasEnded = true;
-    }
-
-    @Override
-    public void render(MatrixStack matrixStack, float tickdelta) {
-    }
-
-    @Override
-    public short getDuration() {
-        return Entropy.getInstance().settings.baseEventDuration;
-    }
-
-    @Override
-    public String type() {
-        return "invisibility";
-    }
-    
 }
