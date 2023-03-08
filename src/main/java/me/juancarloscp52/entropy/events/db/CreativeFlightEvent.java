827a2d8e1471a66085f785ff64e1c9c874249c14
jc <juancarloscp52@gmail.com>
2023-03-08 17:51:16 +0100

    Fix creative flight event disabling flight for creative players

--- src/main/java/me/juancarloscp52/entropy/events/db/CreativeFlightEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/CreativeFlightEvent.java
@@ -6,35 +6,52 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class CreativeFlightEvent extends AbstractTimedEvent {
     @Override
     @Environment(EnvType.CLIENT)
     public void initClient() {
-        MinecraftClient.getInstance().player.getAbilities().allowFlying = true;
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if(null != player) {
+            if (!(player.isCreative() || player.isSpectator())) {
+                player.getAbilities().allowFlying = true;
+            }
+        }
     }
 
     @Override
     @Environment(EnvType.CLIENT)
     public void endClient() {
         super.endClient();
-        MinecraftClient.getInstance().player.getAbilities().allowFlying = false;
-        MinecraftClient.getInstance().player.getAbilities().flying=false;
-        MinecraftClient.getInstance().player.sendAbilitiesUpdate();
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if(null != player){
+            if(!(player.isCreative() || player.isSpectator())){
+                player.getAbilities().allowFlying = false;
+                player.getAbilities().flying=false;
+                player.sendAbilitiesUpdate();
+            }
+        }
     }
 
     @Override
     public void init() {
-        Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> player.getAbilities().allowFlying = true);
+        Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> {
+            if(!(player.isCreative() || player.isSpectator())) {
+                player.getAbilities().allowFlying = true;
+            }
+        });
     }
 
     @Override
     public void end() {
         super.end();
         Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> {
-            player.getAbilities().allowFlying = false;
-            player.getAbilities().flying=false;
-            player.sendAbilitiesUpdate();
+            if(!(player.isCreative() || player.isSpectator())){
+                player.getAbilities().allowFlying = false;
+                player.getAbilities().flying=false;
+                player.sendAbilitiesUpdate();
+            }
         });
     }
 
