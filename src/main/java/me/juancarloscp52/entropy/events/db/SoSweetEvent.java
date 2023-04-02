b0d3da785b6aa3172e337f9a85d3733e5e230450
bl4ckscor3 <bl4ckscor33@gmail.com>
2023-04-03 00:25:28 +0200

    improve so sweet event

--- src/main/java/me/juancarloscp52/entropy/events/db/SoSweetEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/SoSweetEvent.java
@@ -6,11 +6,15 @@ package me.juancarloscp52.entropy.events.db;
 
 import me.juancarloscp52.entropy.Entropy;
 import me.juancarloscp52.entropy.events.AbstractInstantEvent;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Heightmap.Type;
 
 public class SoSweetEvent extends AbstractInstantEvent {
+    private final BlockState sweetBerryBush = Blocks.SWEET_BERRY_BUSH.getDefaultState().with(Properties.AGE_3, 2);;
 
     @Override
     public void init() {
@@ -20,19 +24,16 @@ public class SoSweetEvent extends AbstractInstantEvent {
             var playerPos = serverPlayerEntity.getBlockPos();
 
             for (int ix = -4; ix < 5; ix++) {
-                for (int iy = -4; iy < 5; iy++) {
-                    for (int iz = -4; iz < 5; iz++) {
-                        if ((ix + iy + iz + 100) % 2 == 1)
-                            continue;
+                for (int iz = -4; iz < 5; iz++) {
+                    if ((ix + iz + 100) % 2 == 1)
+                        continue;
 
-                        var blockPos = playerPos.add(ix, iy, iz);
-                        var blockState = world.getBlockState(blockPos);
-                        var downBlockState = world.getBlockState(blockPos.down());
-                        if ((blockState.getBlock().equals(Blocks.AIR) || blockState.canBucketPlace(Fluids.WATER))
-                                && downBlockState.isFullCube(world, blockPos.down())) {
-                            var state = Blocks.SWEET_BERRY_BUSH.getDefaultState().with(Properties.AGE_3, 2);
-                            world.setBlockState(blockPos, state);
-                        }
+                    var blockPos = world.getTopPosition(Type.MOTION_BLOCKING, new BlockPos(playerPos.getX() + ix, 0, playerPos.getZ() + iz));
+                    var blockState = world.getBlockState(blockPos);
+                    var downBlockState = world.getBlockState(blockPos.down());
+                    if ((blockState.getBlock().equals(Blocks.AIR) || blockState.canBucketPlace(Fluids.WATER))
+                            && downBlockState.isFullCube(world, blockPos.down())) {
+                        world.setBlockState(blockPos, sweetBerryBush);
                     }
                 }
             }
