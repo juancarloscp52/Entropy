1782346ce6a4a133c51695c56c1c8e75233a3caf
Juan Carlos <juancarloscp52@gmail.com>
2025-03-01 17:56:55 +0100

    Fix Teleport to Spawn Event (#131)
    * Fix Teleport To Spawn Event when spawn area is not valid
    
    * Fix typo

--- src/main/java/me/juancarloscp52/entropy/EntropyUtils.java
+++ src/main/java/me/juancarloscp52/entropy/EntropyUtils.java
@@ -1,7 +1,11 @@
 package me.juancarloscp52.entropy;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 
 import java.util.Set;
 
@@ -14,5 +18,14 @@ public class EntropyUtils {
 
     public static void teleportPlayer(final ServerPlayerEntity serverPlayerEntity, final Vec3d pos) {
         teleportPlayer(serverPlayerEntity, pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    public static void clearPlayerArea(ServerPlayerEntity serverPlayerEntity){
+        serverPlayerEntity.getWorld().breakBlock(serverPlayerEntity.getBlockPos(), false);
+        serverPlayerEntity.getWorld().breakBlock(serverPlayerEntity.getBlockPos().up(), false);
+        BlockHitResult blockHitResult = serverPlayerEntity.getWorld().raycast(new RaycastContext(serverPlayerEntity.getPos(), serverPlayerEntity.getPos().subtract(0, -6, 0), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.ANY, serverPlayerEntity));
+        if (blockHitResult.getType() == HitResult.Type.MISS || serverPlayerEntity.getWorld().getBlockState(blockHitResult.getBlockPos()).isLiquid()) {
+            serverPlayerEntity.getWorld().setBlockState(serverPlayerEntity.getBlockPos().down(), Blocks.STONE.getDefaultState());
+        }
     }
 }
