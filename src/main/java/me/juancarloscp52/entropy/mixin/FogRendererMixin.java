79951e311f4c7faebb0a8c1775d83466174bd790
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-03-08 16:58:28 +0100

    Fix Herobrine event
    Change fog to black

--- src/main/java/me/juancarloscp52/entropy/mixin/FogRendererMixin.java
+++ src/main/java/me/juancarloscp52/entropy/mixin/FogRendererMixin.java
@@ -17,27 +17,23 @@
 
 package me.juancarloscp52.entropy.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import me.juancarloscp52.entropy.Variables;
-import net.minecraft.client.Camera;
 import net.minecraft.client.renderer.FogParameters;
 import net.minecraft.client.renderer.FogRenderer;
-import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FogRenderer.class)
 public class FogRendererMixin {
 
-    @Inject(method = "setupFog", at = @At(value = "RETURN", ordinal = 1))
-    private static void changeFogDistance(Camera camera, FogRenderer.FogMode fogMode, Vector4f fogColor, float renderDistance, boolean isFoggy, float partialTick, CallbackInfoReturnable<FogParameters> cir, @Local FogRenderer.FogData fogData) {
+    @ModifyReturnValue(method = "setupFog", at = @At(value = "RETURN", ordinal = 1))
+    private static FogParameters changeFogDistance(FogParameters original) {
         if (Variables.customFog) {
-            fogData.start = -150.0F;
-            fogData.end = -100.0F;
+            return new FogParameters(-150.0F, 100.0F, original.shape(), 0.0F, 0.0F, 0.0F, 1.0F);
+        }
+        else {
+            return original;
         }
     }
 
