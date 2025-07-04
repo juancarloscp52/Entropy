106dc2a99dee47ba5a3d6eb7d621912921813d63
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-04 11:11:14 +0200

    Don't draw the progress bar for events with 0 duration

--- src/main/java/me/juancarloscp52/entropy/events/Event.java
+++ src/main/java/me/juancarloscp52/entropy/events/Event.java
@@ -72,7 +72,7 @@ public interface Event {
 
         int size = client.font.width(eventName);
         drawContext.drawString(client.font, eventName, client.getWindow().getGuiScaledWidth() - size - 40, y, CommonColors.WHITE);
-        if (!this.hasEnded()) {
+        if (!this.hasEnded() && getDuration() > 0) {
             drawContext.fill(client.getWindow().getGuiScaledWidth() - 35, y + 1, client.getWindow().getGuiScaledWidth() - 5, y + 8, ARGB.color(150,70, 70, 70));
             drawContext.fill(client.getWindow().getGuiScaledWidth() - 35, y + 1, client.getWindow().getGuiScaledWidth() - 35 + Mth.floor(30 * (getTickCount() / (double) getDuration())), y + 8, ARGB.color(200,255, 255, 255));
         }
