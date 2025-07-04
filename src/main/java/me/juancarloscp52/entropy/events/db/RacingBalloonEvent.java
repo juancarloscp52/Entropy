0532a6f7f211259df926ea1ce6ab106635228734
bl4ckscor3 <bl4ckscor33@gmail.com>
2025-07-04 11:49:18 +0200

    Improve code getting a random harness

--- src/main/java/me/juancarloscp52/entropy/events/db/RacingBalloonEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/RacingBalloonEvent.java
@@ -5,7 +5,6 @@ import me.juancarloscp52.entropy.events.AbstractTimedEvent;
 import me.juancarloscp52.entropy.events.Event;
 import me.juancarloscp52.entropy.events.EventType;
 import net.minecraft.core.Holder;
-import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.Entity;
@@ -28,8 +27,7 @@ public class RacingBalloonEvent extends AbstractTimedEvent {
     public void init() {
         Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> {
             HappyGhast happyGhast = EntityType.HAPPY_GHAST.spawn(player.level(), player.blockPosition(), EntitySpawnReason.EVENT);
-            HolderSet.Named<Item> allHarnesses = BuiltInRegistries.ITEM.getOrThrow(ItemTags.HARNESSES);
-            Item harness = allHarnesses.getRandomElement(player.level().getRandom()).map(Holder::value).orElse(Items.WHITE_HARNESS);
+            Item harness = BuiltInRegistries.ITEM.getRandomElementOf(ItemTags.HARNESSES, player.level().getRandom()).map(Holder::value).orElse(Items.WHITE_HARNESS);
 
             happyGhast.setItemSlot(EquipmentSlot.BODY, harness.getDefaultInstance());
             happyGhast.getAttribute(Attributes.FLYING_SPEED).setBaseValue(0.4D);
