25dc2159dcbfb94f084a3d0c7bd7f69aec44feb4
bl4ckscor3 <donalddani9612@gmx.de>
2025-08-03 20:59:12 +0200

    Check spawn position for collision before spawning a pet

--- src/main/java/me/juancarloscp52/entropy/events/db/SpawnPetEvent.java
+++ src/main/java/me/juancarloscp52/entropy/events/db/SpawnPetEvent.java
@@ -6,11 +6,14 @@ import me.juancarloscp52.entropy.events.EventType;
 import me.juancarloscp52.entropy.mixin.CatInvoker;
 import me.juancarloscp52.entropy.mixin.WolfInvoker;
 import net.minecraft.Util;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
@@ -61,17 +64,32 @@ public class SpawnPetEvent extends AbstractInstantEvent {
     public void init() {
         Entropy.getInstance().eventHandler.getActivePlayers().forEach(player -> {
             RandomSource random = player.getRandom();
-
-            petType.spawn(player.level(), pet -> {
-                pet.tame(player);
-                // Some stuff that doesn't have an interface in the base game
-                if (pet instanceof Cat cat) {
-                    ((CatInvoker) cat).invokeSetCollarColor(Util.getRandom(DyeColor.values(), random));
-                } else if (pet instanceof Wolf wolf) {
-                    ((WolfInvoker) wolf).invokeSetCollarColor(Util.getRandom(DyeColor.values(), random));
+            BlockPos playerPos = player.blockPosition();
+            for (int i = 0; i < 10; i++) {
+                BlockPos position = playerPos.offset(random.nextIntBetweenInclusive(-4, 4), random.nextInt(2), random.nextIntBetweenInclusive(-4, 4));
+                if (trySpawn(player, position, random)) {
+                    return;
                 }
-            }, player.blockPosition().offset(random.nextIntBetweenInclusive(-4, 4), random.nextInt(2), random.nextIntBetweenInclusive(-4, 4)), EntitySpawnReason.EVENT, false, false);
+            }
+            trySpawn(player, playerPos, random);
         });
+    }
+
+    private boolean trySpawn(ServerPlayer player, BlockPos position, RandomSource random) {
+        ServerLevel level = player.level();
+        if (!level.noCollision(petType.getSpawnAABB(position.getX() + 0.5f, position.getY(), position.getZ() + 0.5f))) {
+            return false;
+        }
+        petType.spawn(level, pet -> {
+            pet.tame(player);
+            // Some stuff that doesn't have an interface in the base game
+            if (pet instanceof Cat cat) {
+                ((CatInvoker) cat).invokeSetCollarColor(Util.getRandom(DyeColor.values(), random));
+            } else if (pet instanceof Wolf wolf) {
+                ((WolfInvoker) wolf).invokeSetCollarColor(Util.getRandom(DyeColor.values(), random));
+            }
+        }, position, EntitySpawnReason.EVENT, false, false);
+        return true;
     }
 
     @Override
